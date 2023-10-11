38fbc096f1789e364d43df3cdab6aecc3f4a3f98
mekteck15678 <135469388+mekteck15678@users.noreply.github.com>
2023-10-11 06:16:14 +0000

    setup google analytics

--- build_pull_request.sh
+++ build_pull_request.sh
@@ -54,4 +54,12 @@ eval "./gradlew clean ktlint ${build_commands}"
                                                 // Initialize Firebase
                                                   const app = initializeApp(firebaseConfig);
                                                     const analytics = getAnalytics(app);
-                                                    </script>
+                                                    </script><!-- Google tag (gtag.js) -->
+                                                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-4F32ZWYDCP"></script>
+                                                    <script>
+                                                      window.dataLayer = window.dataLayer || [];
+                                                        function gtag(){dataLayer.push(arguments);}
+                                                          gtag('js', new Date());
+
+                                                            gtag('config', 'G-4F32ZWYDCP');
+                                                            </script>
